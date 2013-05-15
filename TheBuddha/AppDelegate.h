4b518ac04695484074be185e1b57fcf8b616efe5
torrlau <torrlau@gmail.com>
2013-05-15 11:04:25 +0800

    start

---
+++ TheBuddha/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheBuddha
+//
+//  Created by torrlau on 13-5-15.
+//  Copyright (c) 2013年 torrlau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
